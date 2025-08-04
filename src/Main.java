e76f27a2e8ecc7d7f6aa51119b1637959a5703d3
Garry James R. Pagara <gjpagara34848@liceo.edu.ph>
2025-08-04 22:19:47 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
-
-public class SumNumbers {
+public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int total = 0;
+        Book book1 = new Book("The Chronicles of Jupeta", "Jupeta", 150);
+        Book book2 = new Book("Dog Eater", "Jupeta", 180);
+        Book book3 = new Book("Unicorn", "Jupeta", 160);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            int num = input.nextInt();
-            total += num;
-        }
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
 
-        System.out.println("Total Number: " + total);
+        book1.borrowBook();
+        book1.displayInfo();
 
-        input.close();
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
 
