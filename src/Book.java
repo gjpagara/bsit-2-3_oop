ead715935d3a1215c47b913814bb1cfebf9d4de1
Garry James R. Pagara <gjpagara34848@liceo.edu.ph>
2025-08-04 22:20:47 +0800

    Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,1 +1,41 @@
+public class Book {
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
 
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book '" + this.title + "' by " + this.author + " has been added to the library!");
+    }
+
+    public void displayInfo() {
+        System.out.println("---- Book Info ----");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + isAvailable);
+        System.out.println();
+    }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("Book Borrowed\n");
+        } else {
+            System.out.println("Book is currently not available\n");
+        }
+    }
+
+    public void returnBook() {
+        if (!isAvailable) {
+            isAvailable = true;
+            System.out.println("Book Returned\n");
+        } else {
+            System.out.println("Book is already available\n");
+        }
+    }
+}
