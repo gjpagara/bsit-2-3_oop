90c7b0c254c750aebcad7cdfd5f10d34d92a9e84
Garry James R. Pagara <gjpagara34848@liceo.edu.ph>
2025-10-02 16:00:45 +0800

    User.java

--- User.java
+++ User.java
@@ -1,1 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class User {
+    private String userId;
+    private String name;
+    private String email;
+    private List<LibraryItem> borrowedItems;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.borrowedItems = new ArrayList<>();
+    }
+
+    public String getUserId() { return userId; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+
+    public int getBorrowedItemsCount() {
+        return borrowedItems.size();
+    }
+
+    public void addBorrowedItem(LibraryItem item) {
+        borrowedItems.add(item);
+    }
+
+    public void removeBorrowedItem(LibraryItem item) {
+        borrowedItems.remove(item);
+    }
+
+    public void displayBorrowedItems() {
+        if (borrowedItems.isEmpty()) {
+            System.out.println(name + " has no borrowed items.");
+        } else {
+            System.out.println(name + "'s Borrowed Items:");
+            for (LibraryItem item : borrowedItems) {
+                System.out.println("  - " + item.getItemInfo());
+            }
+        }
+    }
+
+    public abstract int getMaxBorrowLimit();
+}
