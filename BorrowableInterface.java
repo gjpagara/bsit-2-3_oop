45fc418bb8cefaf9bcdb2283325259111a63e08a
Garry James R. Pagara <gjpagara34848@liceo.edu.ph>
2025-10-02 15:58:05 +0800

    BorrowableInterface.java

--- BorrowableInterface.java
+++ BorrowableInterface.java
@@ -1,1 +1,10 @@
+interface Borrowable {
+    void borrowItem(String borrowerName);
+    void returnItem();
+    boolean isAvailable();
+    int getBorrowingPeriod();
 
+    default String getBorrowingStatus() {
+        return isAvailable() ? "Available for borrowing" : "Currently borrowed";
+    }
+}
